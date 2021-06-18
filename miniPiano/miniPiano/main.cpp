b1f636d5a822cb2773dcba76d7dde0ee43a9dbcf
adrita1999 <72884537+adrita1999@users.noreply.github.com>
2021-06-19 02:23:22 +0600

    lcd+sensor again merged

--- miniPiano/miniPiano/main.cpp
+++ miniPiano/miniPiano/main.cpp
@@ -2,8 +2,18 @@
  * miniPiano.cpp
  *
  * Created: 17-Jun-21 11:21:48 AM
- * Author : Lenovo
+ * Author : Adrita, Nahian, Rittik
  */ 
+
+#ifndef F_CPU
+#define F_CPU 1000000UL // 16 MHz clock speed
+#endif
+#define D4 eS_PORTD4
+#define D5 eS_PORTD5
+#define D6 eS_PORTD6
+#define D7 eS_PORTD7
+#define RS eS_PORTD2
+#define EN eS_PORTD3
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -204,13 +214,7 @@ int main(void)
 		
 		Lcd4_Set_Cursor(1,1);
 		Lcd4_Write_String(str);
-	while (1)
-	{
-		PORTD=PINA;
 	}
 				
 
 }
-
-
-
